ce5afadf9a71609602e5f42cb159a84b1d34f94f
Sergio Morales <sims1090@gmail.com>
2022-10-24 11:53:10 -0600

    seed fixes

--- src/db/seeds/1666139980684-add-locations.ts
+++ src/db/seeds/1666139980684-add-locations.ts
@@ -3,16 +3,16 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addLocations1666139980684 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO location(locationCode, address, phone1) values('Super Mercado La Union','Villa Fontana Pista Suburabana','(505) 2255-9003') `,
+      `INSERT INTO location(locationName, address, phone1) values('Super Mercado La Union','Villa Fontana Pista Suburabana','(505) 2255-9003') `,
     );
     await queryRunner.query(
-      `INSERT INTO location(locationCode, address, phone1) values('Augusto C Sandino Intl Airport','Km 11 Carretera Norte','(505) 2263-1222/1068') `,
+      `INSERT INTO location(locationName, address, phone1) values('Augusto C Sandino Intl Airport','Km 11 Carretera Norte','(505) 2263-1222/1068') `,
     );
     await queryRunner.query(
-      `INSERT INTO location(locationCode, address, phone1) values('Holiday Inn Convention Center','Pista Juan Pablo Ii','(505) 2255-9002') `,
+      `INSERT INTO location(locationName, address, phone1) values('Holiday Inn Convention Center','Pista Juan Pablo Ii','(505) 2255-9002') `,
     );
     await queryRunner.query(
-      `INSERT INTO location(locationCode, address, phone1) values('Managua Downtown','Pista Juan Pablo Ii','(505) 2255-9001') `,
+      `INSERT INTO location(locationName, address, phone1) values('Managua Downtown','Pista Juan Pablo Ii','(505) 2255-9001') `,
     );
   }
 
